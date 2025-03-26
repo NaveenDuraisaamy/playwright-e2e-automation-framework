357700633504fb78d9bfba0f482d9698d50862a7
naveen <naveenchris77@gmail.com>
2025-03-26 16:57:05 +0530

    Date

--- tests/DatePicker.spec.js
+++ tests/DatePicker.spec.js
@@ -3,11 +3,19 @@ import { test, expect } from "@playwright/test";
 test("Date Picker", async ({ page }) => {
   await page.goto("https://testautomationpractice.blogspot.com/");
 
-const year=2024;
+const year=2025;
 const month="October";
 const date=7;
 
-await page.click("#datepicker");
+while(true){
+ const currentMonth=await page.locator(".ui-datepicker-month").textContent();
 
-await page.waitForTimeout(2000);
+const currentYear=await page.locator(".ui-datepicker-year").textContent();
+
+if(currentMonth===month && currentYear===year){
+  break;
+}
+await page.locator('[title="Next"]').click();
+}
+
 });
