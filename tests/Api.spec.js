41f8c28678f64b0631e8b80c3a14fe254d1fe0f9
naveen <naveenchris77@gmail.com>
2025-04-17 12:29:02 +0530

    new

---
+++ tests/Api.spec.js
@@ -0,0 +1,41 @@
+import{test,expect} from '@playwright/test';
+
+//Get
+test("Get API",async({ request })=>{
+  const response = await request.get("https://reqres.in/api/users?page=2");
+  console.log("GET", await response.json());
+  await expect(response.status()).toBe(200);
+  
+});
+
+//Post
+test("POST API",async({request})=>{
+
+    const postRequest=await request.post("https://reqres.in/api/users",{
+
+        data:{
+            name:"morpheus",
+            job:"leader"
+        }
+    })
+    console.log("POST", await postRequest.json());
+    await expect(postRequest.status()).toBe(201);
+    
+    var req=await postRequest.json();
+    console.log(req.id);
+})
+
+//Put
+test("PUT API", async ({ request }) => {
+  const postRequests= await request.post("https://reqres.in/api/users/2", {
+    data: {
+      name: "morpheus",
+      job: "leader",
+    },
+  });
+  console.log("POST", await postRequest.json());
+  await expect(postRequest.status()).toBe(201);
+
+  var req = await postRequest.json();
+  console.log(req.id);
+});
